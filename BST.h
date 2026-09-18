df2f5cdbd9b5a5235debf2614c06a92d49c53861
mennashemela <mennashemela2006@gmail.com>
2026-09-18 20:30:59 +0300

    Add files via upload

---
+++ BST.h
@@ -0,0 +1,169 @@
+//jana mohamed tamer 20245015
+//menna allah mostafa 20245067
+
+#pragma once
+#ifndef BST_H
+#define BST_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class appointment {
+public:
+    string name;
+    int plevel;
+    string department;
+    appointment(string n, int l, string dept) {
+        name = n;
+        plevel = l;
+        department = dept;
+    }
+};
+
+class Node {
+public:
+    appointment data;
+    Node* left;
+    Node* right;
+    Node(appointment a) : data(a) {
+        left = right = nullptr;
+    }
+};
+class BST {
+    Node* root;
+    Node* insert(Node* node, appointment a) {
+        if (node == nullptr) {
+            return new Node(a);
+        }
+        if (a.plevel < node->data.plevel) {
+            node->left = insert(node->left, a);
+        }
+        else if (a.plevel > node->data.plevel) {
+            node->right = insert(node->right, a);
+        }
+        else {
+            node->left = insert(node->left, a);
+        }
+        return node;
+    }
+    void inorder(Node* node, int& Count) {
+        if (!node) return;
+
+        inorder(node->left, Count);
+
+        cout <<"[" << node->data.name << ","
+            << node->data.plevel << ","
+            << node->data.department<<"]\n";
+        Count++;
+
+        inorder(node->right, Count);
+    }
+    void search(Node* node, int p, int&count) {
+        if (!node) return;
+        search(node->left, p, count);
+        if (node->data.plevel == p) {
+            cout << "[" << node->data.name << ", "
+                << node->data.plevel << ", "
+                << node->data.department << "]\n";
+            count++;
+        }
+        search(node->right, p, count);
+    }
+    Node* findmin(Node* node) {
+        while (node->left)
+            node = node->left;
+        return node;
+    }
+    Node* deleteAll(Node* node, int p, int& count) {
+        if (!node) return nullptr;
+        node->left = deleteAll(node->left, p, count);
+        node->right = deleteAll(node->right, p, count);
+        if (node->data.plevel == p) {
+            count++;
+            if (!node->left) {
+                Node* temp = node->right;
+                delete node;
+                return temp;
+            }
+            if (!node->right) {
+                Node* temp = node->left;
+                delete node;
+                return temp;
+            }
+            Node* temp = findmin(node->right);
+            node->data = temp->data;
+            node->right = deleteAll(node->right, temp->data.plevel, count);
+        }
+        return node;
+    }
+    void moreUrgent(Node* node, int p, int& count) {
+        if (!node) return;
+
+        moreUrgent(node->left, p, count);
+
+        if (node->data.plevel <= p) {
+            cout << "[" << node->data.name << ", "
+                << node->data.plevel << ", "
+                << node->data.department << "]\n";
+            count++;
+        }
+
+        moreUrgent(node->right, p, count);
+    }
+
+    void lessUrgent(Node* node, int p, int& count) {
+        if (!node) return;
+
+        lessUrgent(node->left, p, count);
+
+        if (node->data.plevel >= p) {
+            cout << "[" << node->data.name << ", "
+                << node->data.plevel << ", "
+                << node->data.department << "]\n";
+            count++;
+        }
+
+        lessUrgent(node->right, p, count);
+    }
+public:
+    BST() {
+        root = nullptr;
+    }
+    void schedule(appointment a) {
+        root = insert(root, a);
+        cout << "Appointment Scheduled\n";
+    }
+    void DisplayAll() {
+        int count = 0;
+        inorder(root, count);
+        cout << endl;
+        cout << count << " Appointments are found \n";
+    }
+    void cancelPriority(int p) {
+        int count = 0;
+        root = deleteAll(root, p, count);
+        cout << endl;
+        cout << count << " Appointments are cancelled\n ";
+    }
+    void displayMoreUrgent(int p) {
+        int count = 0;
+        moreUrgent(root, p, count);
+        cout << endl;
+        cout << count << " Appointments found\n ";
+    }
+    void displayLessUrgent(int p) {
+        int count = 0;
+        lessUrgent(root, p, count);
+        cout << endl;
+        cout << count << " Appointments found\n ";
+    }
+    void searchPriority(int p) {
+        int count = 0;
+
+        search(root, p, count);
+
+        cout << endl;
+        cout << count  << "appointments are found";
+    }
+};
+#endif
---
+++ main.cpp
@@ -0,0 +1,93 @@
+//jana mohamed tamer 20245015
+//menna allah mostafa 20245067
+#include <fstream>
+#include "BST.h"
+void showMenu() {
+    cout<< "1. Schedule an appointment \n";
+    cout << "2. Display all appointments \n";
+    cout << "3. Search for an appointment \n";
+    cout << "4. Cancel an appointment \n";
+    cout << "5. Display more urgent than\n";
+    cout << "6. Display less urgent than\n";
+    cout << "0 . Exit...";
+}
+
+int main() {
+    BST tree;
+
+    ifstream inFile("input.txt");
+
+    if (!inFile) {
+        cout << "Error opening file\n";
+        return 1;
+    }
+
+    int n;
+    inFile >> n;
+    inFile.ignore();
+
+    for (int i = 0; i < n; i++) {
+        string name, dept;
+        int priority;
+
+        getline(inFile, name);
+        inFile >> priority;
+        inFile.ignore();
+        getline(inFile, dept);
+
+        tree.schedule(appointment(name, priority, dept));
+    }
+
+    inFile.close();
+
+    int choice;
+    while (true) {
+        showMenu();
+        cout << "\nEnter number of choice: ";
+        cin >> choice;
+        cin.ignore();
+
+        if (choice == 0)
+            break;
+        if (choice == 1) {
+            string name, dept;
+            int p;
+            cout << "Enter patient name: ";
+            getline(cin, name);
+            cout << "Enter priority level: ";
+            cin >> p;
+            cin.ignore();
+            cout << "Enter department name: ";
+            getline(cin, dept);
+            tree.schedule(appointment(name, p, dept));
+        }
+        else if (choice == 2) {
+            tree.DisplayAll();
+        }
+        else if (choice == 3) {
+            int p;
+            cout << "Enter priority level: ";
+            cin >> p;
+            tree.searchPriority(p);
+        }
+        else if (choice == 4) {
+            int p;
+            cout << "Enter priority level: ";
+            cin >> p;
+            tree.cancelPriority(p);
+        }
+        else if (choice == 5) {
+            int p;
+            cout << "Display more urgent than: ";
+            cin >> p;
+            tree.displayMoreUrgent(p);
+        }
+        else if (choice == 6) {
+            int p;
+            cout << "Display less urgent than: ";
+            cin >> p;
+            tree.displayLessUrgent(p);
+        }
+    }
+    return 0;
+}
